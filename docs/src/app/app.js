a44760831e4de8fe636912d23af1fe44ac7ec940
Jeffrey Schiller <jis@qyv.net>
2017-06-06 14:30:46 -0400

    Add Google Analytics (#28)

--- docs/src/app/app.js
+++ docs/src/app/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactGA from 'react-ga';
 import {render} from 'react-dom';
 import {Router, useRouterHistory} from 'react-router';
 import AppRoutes from './AppRoutes';
@@ -13,6 +14,15 @@ window.Perf = require('react-addons-perf');
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
+ReactGA.initialize('UA-28621056-5'); // Initialize Google Analytics
+
+// Function that logs a change in page and sends it to Google via
+// ReactGA
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 /**
  * Render the main app component. You can read more about the react-router here:
  * https://github.com/reactjs/react-router/blob/master/docs/guides/README.md
@@ -20,7 +30,7 @@ injectTapEventPlugin();
 render(
   <Router
     history={useRouterHistory(createHashHistory)({queryKey: false})}
-    onUpdate={() => window.scrollTo(0, 0)}
+    onUpdate={() => {window.scrollTo(0, 0), logPageView()}}
   >
     {AppRoutes}
   </Router>
