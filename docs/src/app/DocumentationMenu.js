d14b81f9e3ebaf5745d8afd6ec84d89bd94ae8f4
Andrew McKinney <andrew.f.mckinney@gmail.com>
2017-06-12 08:28:25 -0400

    Modified and added Documention menubar modified navigation.

---
+++ docs/src/app/DocumentationMenu.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import FlatButton from 'material-ui/FlatButton';
+import Popover, {PopoverAnimationVertical} from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import {Link} from 'react-router';
+import {white} from 'material-ui/styles/colors';
+
+class DocumentationMenu extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleTouchTap = (event) => {
+    // This prevents ghost click.
+    event.preventDefault();
+    this.setState({
+      open: true,
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleRequestClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
+
+  render() {
+    return (
+      <span>
+        <FlatButton
+          style={{color: white, position: 'relative', verticalAlign: 'middle'}}
+          onTouchTap={this.handleTouchTap}
+          label="Documentation"
+          labelPosition="before"
+          icon={<i className="material-icons">arrow_drop_down</i>}
+        />
+        <Popover
+          open={this.state.open}
+          anchorEl={this.state.anchorEl}
+          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          onRequestClose={this.handleRequestClose}
+          animation={PopoverAnimationVertical}
+        >
+          <Menu>
+            <MenuItem
+              primaryText="Devices"
+              primary={false}
+              containerElement={<Link to="/devices/devicesintro" />}
+              onClick={this.handleRequestClose}
+            />
+            <MenuItem
+              primaryText="BluetoothLE"
+              containerElement={<Link to="/bluetoothle/bluetoothleintro" />}
+              onClick={this.handleRequestClose}
+            />
+            <MenuItem
+              primaryText="Arduino 101"
+              containerElement={<Link to="/arduino101/arduino101intro" />}
+              onClick={this.handleRequestClose}
+            />
+            <MenuItem
+              primaryText="BBC micro:bit"
+              containerElement={<Link to="/microbit/microbitintro" />}
+              onClick={this.handleRequestClose}
+            />
+          </Menu>
+        </Popover>
+      </span>
+    );
+  }
+}
+
+export default DocumentationMenu;
