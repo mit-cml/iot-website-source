24ee2c4ef773bc990fb9d8ab1e0b3dd2b7bdc04b
Evan W. Patton <ewpatton@mit.edu>
2017-06-11 22:23:16 -0400

    Update tutorial and howto PDFs from Karen
    Fixes #62

--- docs/src/app/HowTos.js
+++ docs/src/app/HowTos.js
@@ -30,41 +30,71 @@ const howtosList = [
     img: 'assets/howtos/arduino_101.png',
     link: 'assets/howtos/MIT_App_Inventor_Basic_Connection.pdf',
   }, {
+    title: 'How to: Use the Arduino 101\'s Accelerometer',
+    author: 'MIT App Inventor Project',
+    img: 'assets/howtos/arduino_101.png',
+    link: 'assets/howtos/MIT_App_Inventor_IoT_Accelerometer.pdf',
+  }, {
     title: 'How to: Use a Button with Arduino',
     author: 'MIT App Inventor Project',
     img: 'assets/howtos/grove_button.png',
-    link: 'assets/howtos/MIT_App_Inventor_Button.pdf',
+    link: 'assets/howtos/MIT_App_Inventor_IoT_Button.pdf',
+  }, {
+    title: 'How to: Use the Arduino 101\'s Gyroscope',
+    author: 'MIT App Inventor Project',
+    img: 'assets/howtos/arduino_101.png',
+    link: 'assets/howtos/MIT_App_Inventor_IoT_Gyroscope.pdf',
   }, {
     title: 'How to: Use a Humidity Sensor with Arduino',
     author: 'MIT App Inventor Project',
     img: 'assets/howtos/grove_humidity.png',
-    link: 'assets/howtos/MIT_App_Inventor_Humidity_Sensor.pdf',
+    link: 'assets/howtos/MIT_App_Inventor_IoT_Humidity_Sensor.pdf',
   }, {
     title: 'How to: Use a Light Sensor with Arduino',
     author: 'MIT App Inventor Project',
     img: 'assets/howtos/grove_light.png',
-    link: 'assets/howtos/MIT_App_Inventor_Light_Sensor.pdf',
+    link: 'assets/howtos/MIT_App_Inventor_IoT_Light_Sensor.pdf',
   }, {
     title: 'How to: Use a Moisture Sensor with Arduino',
     author: 'MIT App Inventor Project',
     img: 'assets/howtos/grove_moisture.png',
-    link: 'assets/howtos/MIT_App_Inventor_Moisture_Sensor.pdf',
+    link: 'assets/howtos/MIT_App_Inventor_IoT_Moisture_Sensor.pdf',
   }, {
     title: 'How to: Use a Proximity Sensor with Arduino',
     author: 'MIT App Inventor Project',
     img: 'assets/howtos/grove_proximity.png',
-    link: 'assets/howtos/MIT_App_Inventor_Proximity_Sensor.pdf',
+    link: 'assets/howtos/MIT_App_Inventor_IoT_Proximity_Sensor.pdf',
+  }, {
+    title: 'How to: Use a Sound Recorder with Arduino',
+    author: 'MIT App Inventor Project',
+    img: 'assets/howtos/grove_sound_recorder.png',
+    link: 'assets/howtos/MIT_App_Inventor_IoT_Sound_Recorder.pdf',
   }, {
     title: 'How to: Use a RGB LCD with Arduino',
     author: 'MIT App Inventor Project',
     img: 'assets/howtos/grove_rgblcd.png',
     link: 'assets/howtos/MIT_App_Inventor_IoT_RgbLcd.pdf',
   }, {
+    title: 'How to: Use a Temperature Sensor with Arduino',
+    author: 'MIT App Invnetor Project',
+    img: 'assets/howtos/grove_temperature.png',
+    link: 'assets/howtos/MIT_App_Inventor_IoT_Temperature_Sensor.pdf',
+  }, {
+    title: 'How to: Display content on the BBC micro:bit LED matrix',
+    author: 'MIT App Inventor Project',
+    img: 'assets/howtos/microbit_front.png',
+    link: 'assets/howtos/MIT_App_Inventor_IoT_Microbit_LED.pdf',
+  }, {
+    title: 'How to: Use the BBC micro:bit Temperature Sensor',
+    author: 'MIT App Inventor Project',
+    img: 'assets/howtos/microbit_back.png',
+    link: 'assets/howtos/MIT_App_Inventor_IoT_Microbit_Temperature.pdf',
+  }, {
     title: 'LED Control Tutorial',
     author: 'MIT App Inventor Project',
     img: 'assets/howtos/grove_led.png',
     link: 'assets/howtos/MIT_App_Inventor_LED_Control.pdf',
-  },
+  }
 ];
 
 const HowTos = () => (
